515b812c866019639e69da38427967f83e43ea9c
Koppany Papp <koppany@dlabs.hu>
2020-12-23 00:54:24 +0100

    remove all eth networks except etherscan

--- src/networks/nodes/index.js
+++ src/networks/nodes/index.js
@@ -1,19 +1,4 @@
 import ilg from './ilg';
 import ilgt from './ilgt';
 import ethethscan from './eth-ethscan';
-import ethinfura from './eth-infura';
-import ethinfuraws from './eth-infura-ws';
-import ethpocket from './eth-pocket';
-import ethmew from './eth-mew';
-import ethmewws from './eth-mew-ws';
-
-export {
-  ilg,
-  ilgt,
-  ethethscan,
-  ethinfura,
-  ethinfuraws,
-  ethpocket,
-  ethmew,
-  ethmewws
-};
+export { ilg, ilgt, ethethscan };
