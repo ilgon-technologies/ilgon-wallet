6fa8c74b10dbd0ecd5a326f7fdb9b4d1b20fc273
Koppany Papp <koppany@dlabs.hu>
2021-01-27 20:39:18 +0100

    deploy dev script: always build in test mode

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 # builds the wallet and deploys it to the dev server
 # usage: deploy.sh USERNAME
-if [ "$VUE_APP_MODE" != "test" ] && [ "$VUE_APP_MODE" != "prod" ]; then
-  echo "Must provide correct VUE_APP_MODE in environment" 1>&2
-  exit 1
-fi
 if [ "$1" = "" ]; then
   echo "Must provide first arg" 1>&2
   exit 1
 fi
 rm dist.zip
-VUE_APP_MODE="$VUE_APP_MODE" ./build.sh &&
+VUE_APP_MODE="test" ./build.sh &&
   zip -r dist.zip dist &&
   scp ./dist.zip "$1@18.193.173.193:" &&
   ssh \
