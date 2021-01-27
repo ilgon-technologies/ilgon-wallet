7c2a0b9c108fec9dea64cc9bf2c57eea99050a75
Koppany Papp <koppany@dlabs.hu>
2021-01-27 19:09:58 +0100

    fix ILGD homepage

--- src/networks/nodes/ilgd.js
+++ src/networks/nodes/ilgd.js
@@ -1,8 +1,8 @@
-import { ILG, ILGD } from '../types';
+import { ILGD } from '../types';
 
 export default {
   type: ILGD,
-  service: ILG.homePage.replace(/^https?:\/\//, ''),
+  service: ILGD.homePage.replace(/^https?:\/\//, ''),
   url: 'https://wallet.ilgoindev.e-diploma.org/rpc/eth/',
   port: 443,
   auth: false,
