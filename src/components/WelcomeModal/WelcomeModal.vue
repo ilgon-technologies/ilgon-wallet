c53a9a551a2f50aeb0694d9e81d03aeab8cd214a
Koppány Papp Kövecs <koppany@dlabs.hu>
2021-02-17 20:39:02 +0100

    WelcomeModal.vue remove mew references

--- src/components/WelcomeModal/WelcomeModal.vue
+++ src/components/WelcomeModal/WelcomeModal.vue
@@ -15,31 +15,7 @@
       <div class="welcome-modal-container">
         <div class="welcome-modal-text">
           <h3>{{ $t('welcomeMessage.title') }}</h3>
-          <i18n
-            v-if="!firstTimeRu"
-            path="welcomeMessage.desc1"
-            tag="p"
-            for="welcomeMessage.link1"
-          >
-            <a
-              href="https://medium.com/@myetherwallet/lets-make-it-official-mew5-myetherwallet-s-all-new-interface-is-here-2063117180a4"
-              target="_blank"
-              rel="noopener noreferrer"
-              >{{ $t('welcomeMessage.link1') }}</a
-            >
-          </i18n>
-          <p v-if="firstTimeRu">
-            Если Вам удобнее общаться с нами и другими пользователями MEW на
-            русском языке, присоединяйтесь к нашему сообществу в
-            <a target="_blank" href=" https://vk.com/public190491855">ВК</a>!
-            Для поддержки, пишите на
-            <a target="_blank" href="mailto:support@ilgonwallet.com"
-              >support@ilgonwallet.com</a
-            >. Для самых свежих новостей, подпишитесь на наш
-            <a target="_blank" href="https://twitter.com/myetherwallet"
-              >Твиттер</a
-            >."
-          </p>
+          <i18n path="welcomeMessage.desc1" tag="p" />
         </div>
         <div class="welcome-modal-button">
           <button @click="closeModal">
