74a6871798905297e63e0d587046bb10e6e8d444
Koppány Papp Kövecs <koppany@dlabs.hu>
2021-02-18 16:14:10 +0100

    replace ledger referal links

--- src/layouts/ExtensionBrowserAction/components/CxFooter/affiliates.js
+++ src/layouts/ExtensionBrowserAction/components/CxFooter/affiliates.js
@@ -4,7 +4,7 @@ const affiliates = [
     price: '59.00',
     description:
       'A light, yet solid multicurrency hardware wallet for cryptocurrencies. It is easy to carry everywhere thanks to its USB format.',
-    href: 'https://www.ledger.com?r=fa4b',
+    href: 'https://www.ledger.com?r=dd50e17fad63',
     currency: '$',
     logo: 'ledger'
   },
