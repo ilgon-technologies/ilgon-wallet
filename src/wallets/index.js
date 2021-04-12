3c415fb82532495f2c9c73d45d443a80085b299f
Koppány Papp Kövecs <koppany@dlabs.hu>
2021-04-12 20:26:07 +0200

    bugfix and removal of unused code

--- src/wallets/index.js
+++ src/wallets/index.js
@@ -2,17 +2,13 @@ import { LedgerWallet, TrezorWallet } from './hardware';
 import WalletInterface from './WalletInterface';
 import { MnemonicWallet } from './software';
 import {
-  MewConnectWallet,
   WalletConnectWallet,
-  WalletLinkWallet
 } from './hybrid';
 
 export {
   LedgerWallet,
   TrezorWallet,
-  MewConnectWallet,
   WalletConnectWallet,
-  WalletLinkWallet,
   WalletInterface,
   MnemonicWallet
 };
