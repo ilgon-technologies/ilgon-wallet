16bc324dc65720c34a7fff3b10c5e6878138115f
Koppány Papp Kövecs <koppany@dlabs.hu>
2021-05-14 20:17:20 +0200

    build.sh don't depend on changes in package.json

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ if [ "$VUE_APP_MODE" != "test" ] && [ "$VUE_APP_MODE" != "prod" ]; then
   echo "Must provide correct VUE_APP_MODE in environment" 1>&2
   exit 1
 fi
-NODE_OPTIONS="--max-old-space-size=4096" npm run build:ci
+NODE_OPTIONS="--max-old-space-size=4096" npm run update:lists && vue-cli-service build
