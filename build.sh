0e6931d8265130cb596f05da1cdadf3f938a098e
Koppány Papp Kövecs <koppany@dlabs.hu>
2021-05-14 22:31:05 +0200

    do not depend on branch 4

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ if [ "$VUE_APP_MODE" != "test" ] && [ "$VUE_APP_MODE" != "prod" ]; then
   echo "Must provide correct VUE_APP_MODE in environment" 1>&2
   exit 1
 fi
-NODE_OPTIONS="--max-old-space-size=4096" npm run build:ci
+npm run update:lists && NODE_OPTIONS="--max-old-space-size=4096" vue-cli-service build
